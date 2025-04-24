c75a2b2500f13879af55be494140c1d8415c15fa
Wil Sevilla <45079501+WdSevilla@users.noreply.github.com>
2025-04-23 19:46:19 -0600

    cambio imagenes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+
 import tailwindcss from '@tailwindcss/vite';
+
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  output: 'server',
+
   adapter: netlify()
 });
